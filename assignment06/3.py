fb3f6dc12fa29618804623da01e1624f1cc09758
unknown <harirnaghshband8013@gmail.com>
2023-01-25 05:53:03 +0330

    assign

--- assignment06/3.py
+++ assignment06/3.py
@@ -1,16 +1,16 @@
 def cubic(a,b,c,d):
-    u = [1] 
-    u.append(complex(-.5,3**(1/2)/2))
-    u.append(complex(-.5,-1*3**(1/2)/2))
+    h = [1] 
+    h.append(complex(-.5,3**(1/2)/2))
+    h.append(complex(-.5,-1*3**(1/2)/2))
 
     delta = 18*a*b*c*d - 4*b**3*d + b**2*c**2 - 4*a*c**3 - 27*a**2*d**2
     delta0 = b**2 - 3*a*c
     delta1 = 2*b**3 - 9*a*b*c + 27*a**2*d
-    C = delta1 + (delta1**2 - 4*delta0**3)**(1/2)
-    C = (C/2)**(1/3)
+    c = ((delta1 + (delta1**2 - 4*delta0**3)**(1/2))/2)**(1/3)
+
     l = []
     for i in range(3):
-        x = b + u[i]*C + delta0/(u[i]*C)
+        x = b + h[i]*c + delta0/(h[i]*c)
         l.append(x/(-3*a))
 
     return l
